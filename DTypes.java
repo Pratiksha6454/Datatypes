1a855d46341fb7317d5b76afc073dd287e949d32
Pratiksha <pratikshadadhwal842@gmail.com>
2024-10-25 19:42:57 -0700

    Input output added

---
+++ DTypes.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class DTypes{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);//We have to create object of scanner class to take input
+        System.out.println("Enter the name of your college");
+        String name=sc.nextLine();
+        System.out.println("Your University name is :"+name);
+    }
+}
