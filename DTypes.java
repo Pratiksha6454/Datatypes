bb9c10e614471cd073229f7851b87531393e3c41
Pratiksha <pratikshadadhwal842@gmail.com>
2024-10-25 19:48:18 -0700

    DataTypes main topic done

--- DTypes.java
+++ DTypes.java
@@ -5,5 +5,10 @@ public class DTypes{
         System.out.println("Enter the name of your college");
         String name=sc.nextLine();
         System.out.println("Your University name is :"+name);
+        // Lets learn about DataTypes
+        int age=20;
+        char section='A';
+        float percentage=30.4f;
+        long bigData=2378273;
     }
 }
